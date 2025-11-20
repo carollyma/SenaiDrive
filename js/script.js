3bca573b13724c6633bf30dd3986a7f5b3110d7f
carollyna <carollynaalima@gmail.com>
2025-11-19 22:22:43 -0300

    continuação

--- js/script.js
+++ js/script.js
@@ -10,7 +10,73 @@ selectMarca.addEventListener("change", function() {
         if(marcaSelecionada === "" || titulo.includes(marcaSelecionada)) {
         card.style.display = "flex"
         } else {
+            //Se a condição for falsa (a marca do carro NÃo corresponde à selecionada),
+            // o card é ocultado (display: none).
             card.style.display = "none"
         }
     })
 })
+
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+
+    let slides = document.getElementsByClassName("meuSlide");
+    let dots = document.getElementsByClassName("dot");
+
+    if(n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if(n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex -1].style.display = "block";
+    dots[slideIndex -1].className += " active";
+}
+
+let autoSlideIndex =0;
+carouselAutomatico();
+
+function carouselAutomatico() {
+    let i;
+    let slides = document.getElementsByClassName("meuSlide");
+    let dots = document.getElementsByClassName("dot");
+
+    for (i =0; i < slides.length; i++){
+        slides[i].style.display = "none"; 
+    }
+
+    autoSlideIndex++;
+
+    if (autoSlideIndex > slides.length) {
+        autoSlideIndex = 1;
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active"
+
+    setTimeout(carouselAutomatico(), 5000)
+        
+    
+}
